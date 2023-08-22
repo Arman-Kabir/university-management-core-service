489561f5cd592a01758aeb49c62dc12e6b4684d1
Arman-Kabir <armankabir8@gmail.com>
2023-08-22 14:57:09 +0600

    zod validation added to create route

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 const router = express.Router();
 
-router.post('/',AcademicSemesterController.insertIntoDB)
+router.post(
+    '/',
+    validateRequest(AcademicSemesterValidation.create),
+    AcademicSemesterController.insertIntoDB
+)
 
 
 export const AcademicSemesterRoutes = router;
