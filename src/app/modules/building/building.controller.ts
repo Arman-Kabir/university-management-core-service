5836e956f4f449ef8beb31dc84a5afd90e132de5
Arman-Kabir <armankabir8@gmail.com>
2023-08-28 22:51:07 +0600

    building data filtering

--- src/app/modules/building/building.controller.ts
+++ src/app/modules/building/building.controller.ts
@@ -3,6 +3,8 @@ import catchAsync from "../../../shared/catchAsync";
 import { BuildingService } from "./building.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { buildingFilterableFields } from "./building.constants";
+import pick from "../../../shared/pick";
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     // console.log(req.body);
@@ -14,6 +16,26 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
         data: result
     })
 });
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    console.log(req.query);
+
+    const filters = pick(req.query, buildingFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+    const result = await BuildingService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Building fetched successfully",
+        meta:result.meta,
+        data: result.data
+    })
+});
+
+
+
 export const BuildingController = {
-    insertIntoDB
+    insertIntoDB,
+    getAllFromDB
 }
--- src/app/modules/building/building.service.ts
+++ src/app/modules/building/building.service.ts
@@ -1,5 +1,10 @@
-import { Building } from "@prisma/client";
+import { Building, Prisma } from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import { IBuildingFilterRequest } from "./building.interface";
+import { IPaginationOptions } from "../../../interfaces/pagination";
+import { paginationHelpers } from "../../../helpers/paginationHelper";
+import { buildingSearchAbleFields } from "./building.constants";
+import { IGenericResponse } from "../../../interfaces/common";
 
 // const prisma = new PrismaClient();
 
@@ -7,11 +12,64 @@ const insertIntoDB = async (data: Building): Promise<Building> => {
     // console.log(data);
     const result = await prisma.building.create({
         data
-    
+
     })
     return result;
 };
 
-export const BuildingService={
-    insertIntoDB
+const getAllFromDB = async (
+    filters: IBuildingFilterRequest,
+    options: IPaginationOptions
+
+): Promise<IGenericResponse<Building[]>> => {
+    const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+    const { searchTerm } = filters;
+    const andConditions = [];
+
+    if (searchTerm) {
+        andConditions.push({
+            OR: buildingSearchAbleFields.map((field) => ({
+                [field]: {
+                    contains: searchTerm,
+                    mode: 'insensitive'
+                }
+            }))
+        })
+    }
+
+    const whereConditions: Prisma.BuildingWhereInput =
+        andConditions.length > 0 ? { AND: andConditions } : {};
+
+    const result = await prisma.building.findMany({
+        skip,
+        take: limit,
+        where: whereConditions,
+        orderBy: options.sortBy && options.sortOrder
+            ? {
+                [options.sortBy]: options.sortOrder
+            }
+            : {
+                createdAt: 'desc'
+            }
+    });
+
+    const total = await prisma.building.count({
+        where: whereConditions
+    });
+    
+    return {
+        meta: {
+            page,
+            limit,
+            total
+        },
+        data: result
+    };
+}
+
+
+
+export const BuildingService = {
+    insertIntoDB,
+    getAllFromDB
 }
