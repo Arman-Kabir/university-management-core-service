d85626f2800ac00ba6a75056b08a4c48d2f064b2
Arman-Kabir <armankabir8@gmail.com>
2023-08-28 22:59:44 +0600

    building create validation and role-based api access done

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import { BuildingController } from './building.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { BuildingValidations } from './building.validations';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-router.get('/',BuildingController.getAllFromDB)
+router.get('/', BuildingController.getAllFromDB)
 
-router.post('/', BuildingController.insertIntoDB)
+router.post(
+    '/',
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    validateRequest(BuildingValidations.create),
+    BuildingController.insertIntoDB)
 
 
 export const buildingRoutes = router;
