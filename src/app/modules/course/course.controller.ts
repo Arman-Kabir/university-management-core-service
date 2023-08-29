b3389f1f7127f1acf032d655767b15f98835f3aa
Arman-Kabir <armankabir8@gmail.com>
2023-08-29 12:32:54 +0600

    course created and prerequisite set

--- src/app/modules/course/course.controller.ts
+++ src/app/modules/course/course.controller.ts
@@ -4,13 +4,15 @@ import { CourseService } from "./course.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 
-const insertIntoDB = catchAsync(async(req:Request,res:Response)=>{
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+    console.log(req.body);
+
     const result = await CourseService.insertIntoDB(req.body);
-    sendResponse(res,{
-        statusCode:httpStatus.OK,
-        success:true,
-        message:"Course created successfully",
-        data:result
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Course created successfully",
+        data: result
     });
 });
 
--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,10 +1,30 @@
 import { Course } from "@prisma/client";
 import prisma from "../../../shared/prisma";
 
-const insertIntoDB = async (data: Course): Promise<Course> => {
+const insertIntoDB = async (data: any): Promise<any> => {
+    const { preRequisiteCourses, ...courseData } = data;
+
+    console.log(preRequisiteCourses);
+    console.log(courseData);
+
     const result = await prisma.course.create({
-        data
+        data: courseData
     });
+
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+        for (let index = 0; index < preRequisiteCourses.length; index++) {
+            const createPrerequisite = await prisma.courseToPreRequisite.create({
+                data:{
+                    courseId:result.id,
+                    preRequisiteId:preRequisiteCourses[index].courseId
+                }
+            });
+            console.log(createPrerequisite);
+        }
+    }
+
+
+
     return result;
 };
 
